b2cc5b75a1a21e5c0871248a67066471a2eddd7b
Ameyaa Chivukula <87368511+ameyaac@users.noreply.github.com>
2025-03-01 08:53:19 +0000

    Copilot Third Commit

--- comments.js
+++ comments.js
@@ -1,40 +1,37 @@
-// Create Web Server
+// create a web server
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
+// load the http module
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var qs = require('querystring');
+var comments = [];
 
-app.use(bodyParser.json());
+// create an http server
+http.createServer(function (req, res) {
+    // parse the request containing file name
+    var pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-app.get('/comments', (req, res) => {
-  fs.readFile('./comments.json', 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Could not read comments file.');
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.post('/comments', (req, res) => {
-  fs.readFile('./comments.json', 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Could not read comments file.');
-    } else {
-      const comments = JSON.parse(data);
-      comments.push(req.body);
-      fs.writeFile('./comments.json', JSON.stringify(comments), err => {
+    // read the requested file content from file system
+    fs.readFile(pathname.substr(1), function (err, data) {
         if (err) {
-          res.status(500).send('Could not write comments file.');
+            console.log(err);
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            res.writeHead(404, {'Content-Type': 'text/html'});
         } else {
-          res.send('Comment added.');
+            // Page found
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            // write the content of the file to response body
+            res.write(data.toString());
         }
-      });
-    }
-  });
-});
+        // send the response body
+        res.end();
+    });
+}).listen(8080);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+console.log('Server running at http:// ' + 'localhost:8080/');
