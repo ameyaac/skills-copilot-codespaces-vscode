5627f0150ec389b15fb6e879b39b171bc46ce606
Ameyaa Chivukula <87368511+ameyaac@users.noreply.github.com>
2025-03-01 06:59:09 +0000

    Copilot First Commit

---
+++ skills.js
@@ -0,0 +1,6 @@
+function calculateNumbers(var1, var2) {
+  return var1 + var2;
+}
+console.log(calculateNumbers(2, 3)); // 5
+console.log(calculateNumbers(5, 5)); // 10
+console.log(calculateNumbers(10, 10)); // 20
