512f8db30706bd68aba3b0deeb9c17a3c72b2998
Ameyaa Chivukula <87368511+ameyaac@users.noreply.github.com>
2025-03-01 09:14:35 +0000

    Copilot Third Commit

--- comments.js
+++ comments.js
@@ -1,13 +1,14 @@
+// create a web server
+
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
 const PORT = 3000;
-const COMMENTS_FILE = 'comments.json'; // Ensure this file exists in your online editor
 const VIEWS_PATH = 'views';
-const PUBLIC_PATH = 'public';
 
+// Function to read file and serve content
 const readFile = (filePath, res, contentType = 'text/html') => {
   fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -19,49 +20,28 @@ const readFile = (filePath, res, contentType = 'text/html') => {
   });
 };
 
-const handleRoute = (pathname, req, res) => {
+// Function to handle routes
+const handleRoute = (pathname, res) => {
   switch (pathname) {
     case '/':
       readFile(path.join(VIEWS_PATH, 'index.html'), res);
       break;
-    case '/post':
-      readFile(path.join(VIEWS_PATH, 'post.html'), res);
+    case '/about':
+      readFile(path.join(VIEWS_PATH, 'about.html'), res);
       break;
-    case '/pinglun':
-      handleComment(req, res);
+    case '/contact':
+      readFile(path.join(VIEWS_PATH, 'contact.html'), res);
       break;
     default:
-      if (pathname.startsWith('/public/')) {
-        const ext = path.extname(pathname).slice(1);
-        const contentType = ext === 'css' ? 'text/css' : 'application/javascript';
-        readFile(path.join(PUBLIC_PATH, pathname), res, contentType);
-      } else {
-        readFile(path.join(VIEWS_PATH, '404.html'), res);
-      }
+      readFile(path.join(VIEWS_PATH, '404.html'), res);
       break;
   }
 };
 
-const handleComment = (req, res) => {
-  const comment = url.parse(req.url, true).query;
-  comment.dateTime = new Date().toISOString();
-  fs.readFile(COMMENTS_FILE, (err, data) => {
-    let comments = [];
-    if (!err) {
-      comments = JSON.parse(data);
-    }
-    comments.unshift(comment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments), (err) => {
-      if (err) throw err;
-      res.writeHead(302, { 'Location': '/' });
-      res.end();
-    });
-  });
-};
-
+// Create the HTTP server
 http.createServer((req, res) => {
   const pathname = url.parse(req.url, true).pathname;
-  handleRoute(pathname, req, res);
+  handleRoute(pathname, res);
 }).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
