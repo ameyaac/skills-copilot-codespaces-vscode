b3d6394f20c1d5bf1b77e4f0af3e4b4ac433114b
Ameyaa Chivukula <87368511+ameyaac@users.noreply.github.com>
2025-03-01 07:15:51 +0000

    Copilot Third Commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create Web Server
+// Create Web Server
+const express = require('express');
+const app = express();
+
+// Create route
+app.get('/comments', (req, res) => {
+  res.send('This is the comment page');
+});
+
+// Create route
+app.get('/comments/new', (req, res) => {
+  res.send('This is the new comment page');
+});
+
+// Create route
+app.get('/comments/:id', (req, res) => {
+  res.send('This is the comment ' + req.params.id + ' page');
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server has started');
+});
